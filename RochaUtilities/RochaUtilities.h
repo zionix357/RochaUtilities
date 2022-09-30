840d991a7357bfdccb3bc9718786035b06a995a7
Arthur Rocha <arthur_stads@hotmail.com>
2022-09-30 20:39:39 -0300

    Initial Commit

---
+++ RochaUtilities/RochaUtilities.h
@@ -0,0 +1,18 @@
+//
+//  RochaUtilities.h
+//  RochaUtilities
+//
+//  Created by Arthur Rocha on 30/09/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for RochaUtilities.
+FOUNDATION_EXPORT double RochaUtilitiesVersionNumber;
+
+//! Project version string for RochaUtilities.
+FOUNDATION_EXPORT const unsigned char RochaUtilitiesVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RochaUtilities/PublicHeader.h>
+
+
